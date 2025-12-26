67bb8e1e2e1d5226c314c1df489dda55a47d9a7c
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2025-12-26 22:35:04 +0600

    Add a program to print your name and details

---
+++ Input Output Assignment/Write a program to print your name and details/code.c
@@ -0,0 +1,7 @@
+// Write a program to print your name and details
+
+#include<stdio.h>
+int main () {
+    printf("My name is : Md. Abirul Islam Pritom\n CSE Student \n Daffodil International University");
+    return 0;
+}
