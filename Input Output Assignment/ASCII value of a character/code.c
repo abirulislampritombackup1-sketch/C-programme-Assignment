a1bcc5ee60df11a4ec55248a67b5891357be6e00
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2025-12-27 10:00:36 +0600

    Add the main file

---
+++ Input Output Assignment/ASCII value of a character/code.c
@@ -0,0 +1,10 @@
+//  ASCII VALUE OF A CHARACTER
+
+#include<stdio.h>
+int main () {
+    char a;
+    printf("Enter a character :");
+    scanf(" %c",&a);
+    printf("The ASCII VALUE  of the character is : %d",a);
+    return 0;
+}
