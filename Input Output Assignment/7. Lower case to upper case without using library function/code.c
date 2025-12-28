9d823b4511fcc0055709c86bf1136c5a83123a8f
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2025-12-28 22:40:52 +0600

    Add the main code

---
+++ Input Output Assignment/7. Lower case to upper case without using library function/code.c
@@ -0,0 +1,10 @@
+// Lowercase to Uppercase letter without using library function
+
+#include<stdio.h>
+int main (){
+    char ch;
+    printf("Enter the Lowercase letter :");
+    scanf("%c",&ch);
+    printf("The Uppercase letter is :%c",ch-32);
+    return 0;
+}
