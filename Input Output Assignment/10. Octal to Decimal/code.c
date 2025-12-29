725430687b6b38ab4693bc65a89cc4a77b673ec1
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2025-12-29 10:58:09 +0600

    Add the main code

---
+++ Input Output Assignment/10. Octal to Decimal/code.c
@@ -0,0 +1,10 @@
+// Code for octal to decimal
+
+#include<stdio.h>
+int main () {
+    int n;
+    printf("Enter the Octal number:");
+    scanf("%o",&n);
+    printf("The decimal number is :%d",n);
+    return 0;
+}
