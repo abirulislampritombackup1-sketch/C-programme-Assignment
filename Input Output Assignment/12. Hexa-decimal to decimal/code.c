d0205fb1a09df4039a32f213cdbe91cccd570be0
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2025-12-29 17:09:24 +0600

    add main file of hexa-decimal to decimal number

---
+++ Input Output Assignment/12. Hexa-decimal to decimal/code.c
@@ -0,0 +1,10 @@
+// C program for hexa-decimal to decimal
+
+#include<stdio.h>
+int main() {
+    int n;
+    printf("Enter the hexa-decimal number:");
+    scanf("%x",&n);
+    printf("The decimal number is :%d",n);
+    return 0;
+}
