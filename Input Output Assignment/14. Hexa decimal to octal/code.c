f30f38e83e1f5b4eca9fd0d2a332797a2a0c261a
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2025-12-29 21:22:41 +0600

    add the main file of hexadecimal to octal

---
+++ Input Output Assignment/14. Hexa decimal to octal/code.c
@@ -0,0 +1,10 @@
+// C programme for hexadcimal to octal
+
+#include<stdio.h>
+int main () {
+    int n;
+    printf("Enter a hexadecimal number :");
+    scanf("%x",&n);
+    printf("The octal number is :%o",n);
+    return 0;
+}
