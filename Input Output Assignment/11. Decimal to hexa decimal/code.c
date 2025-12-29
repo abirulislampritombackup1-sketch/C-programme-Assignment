54daf179d1e5ead8c7d28ea0d2bf25415e8a42bb
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2025-12-29 11:25:46 +0600

    Add the main code of decimal to hexa decimal

---
+++ Input Output Assignment/11. Decimal to hexa decimal/code.c
@@ -0,0 +1,10 @@
+// C programme for decimal to hexadecimal
+
+#include<stdio.h>
+int main () {
+    int n;
+    printf("Enter decimal number:");
+    scanf("%d",&n);
+    printf("The hexa decimal format is :%x",n);
+    return 0;
+}
