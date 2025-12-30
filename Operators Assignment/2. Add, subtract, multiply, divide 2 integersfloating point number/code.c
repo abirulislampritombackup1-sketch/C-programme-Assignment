d23d8d6853547aed3187df051e9f06184dc1b0c3
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2025-12-30 12:25:59 +0600

    add the main code of add, subtraction, multiplication and division

---
+++ Operators Assignment/2. Add, subtract, multiply, divide 2 integersfloating point number/code.c
@@ -0,0 +1,13 @@
+// C programme for add,subtraction, multiplication,division of two float
+
+#include<stdio.h>
+int main () {
+    float a,b;
+    printf("Enter two float numbers:");
+    scanf("%f %f",&a,&b);
+    printf("The sum of two float = %f \n",a+b);
+    printf("The subtraction of two float = %f \n",a-b);
+    printf("The multiplication of two float = %f \n",a*b);
+    printf("The division of two float = %f \n",a/b);
+    return 0;
+}
