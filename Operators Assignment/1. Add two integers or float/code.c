801e182044c45e64e8da052b19238896b910fdb2
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2025-12-30 12:00:26 +0600

    add the main file of sum of two numbers

---
+++ Operators Assignment/1. Add two integers or float/code.c
@@ -0,0 +1,10 @@
+// Add two integers or float 
+
+#include<stdio.h>
+int main (){
+    int a,b;
+    printf("Enter two numbers :");
+    scanf("%d %d",&a,&b);
+    printf("The sum of two numbers = %d",a+b);
+    return 0;
+}
