7a64ce69f4cc9f5b3449d07a60ebd5330b513705
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2025-12-30 21:19:14 +0600

    add the main file of area of a circle

---
+++ Operators Assignment/6. Calculate the area of a circle/code.c
@@ -0,0 +1,11 @@
+// area of a circle
+
+#include<stdio.h>
+int main () {
+    float a;
+    printf("Enter the radias of the circle:");
+    scanf("%f",&a);
+    float area = 3.1416*a*a;
+    printf("The are of the circle is : %f",area);
+    return 0;
+}
