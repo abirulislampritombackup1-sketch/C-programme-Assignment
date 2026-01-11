4270da4e8f142ea1a38aaa4d86a9f3ec0611cf03
Md. Abirul Islam Pritom <abirulislampritombackup1@gmail.com>
2026-01-11 07:59:24 +0600

    add the main file of vowel or consonant

---
+++ If-else/11. read any letter and display vowel or consonant/code.c
@@ -0,0 +1,14 @@
+// read any letter and display vowel or consonant.
+
+#include<stdio.h>
+int main () {
+    char ch;
+    printf("Enter any character: ");
+    scanf("%c",&ch);
+    if ( ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='u' || ch=='U' ) {
+        printf("Vowel\n");
+    } else {
+        printf("Consonant\n");
+    }
+    return 0;
+}
